d70d7c9621acd4f42f7c88d1e36ad5d9668e9cc3
BurgerRelish <23640103@sun.ac.za>
2023-10-17 23:16:54 +0200

    JWT

--- auth.py
+++ auth.py
@@ -24,31 +24,31 @@ def decode_jwt(token: str) -> dict:
         print(e)
         return {}
     
-def verify_jwt(self, jwtoken: str) -> bool:
-    try:
-        jwt.decode(jwtoken, JWT_SECRET, algorithms=[JWT_ALGORITHM])
-        return True
-    except ExpiredSignatureError:
-        return False  # Token has expired
-    except DecodeError:
-        return False  # Token is invalid
-
 class JWTBearer(HTTPBearer):
     def __init__(self, auto_error: bool = True):
-        super().__init__(auto_error=auto_error)
+        super(JWTBearer, self).__init__(auto_error=auto_error)
 
     async def __call__(self, request: Request):
-        credentials: HTTPAuthorizationCredentials = await super().__call__(request)
+        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
         if credentials:
-            if credentials.scheme != "Bearer":
-                raise HTTPException(status_code=401, detail="Invalid authentication scheme.")
-            if not verify_jwt(credentials.credentials):
-                raise HTTPException(status_code=401, detail="Invalid token or expired token.")
+            if not credentials.scheme == "Bearer":
+                raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
+            if not self.verify_jwt(credentials.credentials):
+                raise HTTPException(status_code=403, detail="Invalid token or expired token.")
             return credentials.credentials
         else:
-            raise HTTPException(status_code=401, detail="Invalid authorization code.")
+            raise HTTPException(status_code=403, detail="Invalid authorization code.")
 
+    def verify_jwt(self, jwtoken: str) -> bool:
+        isTokenValid: bool = False
 
+        try:
+            payload = decode_jwt(jwtoken)
+        except:
+            payload = None
+        if payload:
+            isTokenValid = True
+        return isTokenValid
         
         
 def encode_broker_jwt(payload: dict[str, any]) -> str:
